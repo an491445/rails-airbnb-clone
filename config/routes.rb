b68756fb27cac594d04b5edc61e0d61908c6c092
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2017-03-27 17:26:10 +0200

    minivans controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'minivans/index'
+
+  get 'minivans/show'
+
+  get 'minivans/new'
+
+  get 'minivans/create'
+
+  get 'minivans/edit'
+
+  get 'minivans/update'
+
+  get 'minivans/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
