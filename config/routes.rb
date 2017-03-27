3f00a94258b4ac8b7c42f80c03221bf70b38cb8e
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2017-03-27 17:53:15 +0200

    routes modifs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-
+resources:minivans do
+  resources :bookings, only[:create, :index]
+end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
