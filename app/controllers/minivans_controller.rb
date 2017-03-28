c188eeabe1ccc7d91560e97746ab46fa4ac2eeeb
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2017-03-28 17:08:19 +0200

    controller, show page  and front-end

--- app/controllers/minivans_controller.rb
+++ app/controllers/minivans_controller.rb
@@ -1,12 +1,13 @@
 class MinivansController < ApplicationController
   before_action :set_user, only: [:new, :create]
+  before_action :set_minivan, only: [:show, :edit, :update]
 
   def index
     @minivans = Minivan.all
   end
 
   def show
-    @minivan = Minivan.find(params[:id])
+    #@minivan = Minivan.find(params[:id])
   end
 
   def new
@@ -14,19 +15,22 @@ class MinivansController < ApplicationController
   end
 
   def create
-     @minivan = Minivan.create(minivans_params)
-     @minivan.user= @user
-      if @minivan.save
-        redirect_to user_path(@user)
-      else
-        render :new
-      end
+    @minivan = Minivan.new(minivans_params)
+    @minivan.user= @user
+    if @minivan.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def edit
+    #@minivan = Minivan.find(params[:id])
   end
 
   def update
+    #@minivan = Minivan.find(params[:id])
+    @minivan.update(minivans_params)
   end
 
   def destroy
@@ -37,7 +41,11 @@ private
     @user = User.find(params[:user_id])
   end
 
+  def set_minivan
+    @minivan = Minivan.find(params[:id])
+  end
+
   def minivans_params
-    params.require(:minivan).permit(:capacity, :model_year, :gear_box, :price_per_day, :location, :description, :picture, :photo_cache)
+    params.require(:minivan).permit(:capacity, :model_year, :gearbox, :price_per_day, :location, :description, :picture, :photo_cache)
   end
 end
