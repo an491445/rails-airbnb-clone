3f3eaaf9043c32e0c0602c0e641dfdf837420cc9
Anu Maria Kokkarinen <anu-maria@hotmail.com>
2017-03-29 12:18:27 +0200

    home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @banner_photo = "pexels-minivan_hspdl5.jpg"
+    @minivans = Minivan.limit(5)
   end
 end
