8838fb92ecc5667200887b37480ff396edbbeb18
Anu Maria Kokkarinen <anu-maria@hotmail.com>
2017-03-30 16:19:54 +0200

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :minivans, only: [:show, :index] do
+  resources :minivans, only: [:index] do
     resources :bookings, only: [:create, :show, :edit, :update, :destroy]
   end
 
   resources :bookings, only: [:show]
 
   resources :users, only: [:index, :show] do
-    resources :minivans, only: [:new, :create, :update, :edit, :destroy]
+    resources :minivans, only: [:show, :new, :create, :update, :edit, :destroy]
   end
 
   resource :profile, only: [:edit, :update, :show]
 
-  devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
