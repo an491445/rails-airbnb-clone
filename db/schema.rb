ad66cee27c4d23b6d032bbac1dea6793898ea324
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2017-03-31 10:23:48 +0200

    adding frontpage for login, signin and creating reviez model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170330101542) do
+ActiveRecord::Schema.define(version: 20170331080857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,16 @@ ActiveRecord::Schema.define(version: 20170330101542) do
     t.index ["user_id"], name: "index_profiles_on_user_id", using: :btree
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "booking_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_reviews_on_booking_id", using: :btree
+    t.index ["user_id"], name: "index_reviews_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -100,4 +110,6 @@ ActiveRecord::Schema.define(version: 20170330101542) do
   add_foreign_key "bookings", "users"
   add_foreign_key "departures", "minivans"
   add_foreign_key "minivans", "users"
+  add_foreign_key "reviews", "bookings"
+  add_foreign_key "reviews", "users"
 end
